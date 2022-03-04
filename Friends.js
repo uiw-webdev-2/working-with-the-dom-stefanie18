73afcf0a994822892c72e62480c360cff23b9fda
stefanie18 <89417960+stefanie18@users.noreply.github.com>
2022-03-03 21:24:02 -0600

    revised

---
+++ Friends.js
@@ -0,0 +1,16 @@
+class Friends {
+  constructor(
+    name,
+    sofa,
+    pictureFrame,
+    image
+  ) {
+    this.name = name;
+    this.sofa = sofa;
+    this.pictureFrame = pictureFrame;
+    this.image = image;
+  }
+}
+
+
+export default Friends;
